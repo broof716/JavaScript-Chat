40b0fdb53e7efcd6f500a5992da1ab3543ca7ead
Bryan Roof <broof716@gmail.com>
2021-03-03 16:47:04 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="44cdb951-0ee0-4ff3-8a84-70ce68f20fda"
+      userName="JavaScript"
+      userSecret="1234"
+    />
+  );
+}
+
+export default App;
