acfb2bcc166de722bcae23261e27a2f7a0d13c07
Bryan Roof <broof716@gmail.com>
2021-03-03 17:13:19 -0700

    incorperated chatengine.io api to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ChatEngine } from 'react-chat-engine';
-
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
       projectID="44cdb951-0ee0-4ff3-8a84-70ce68f20fda"
       userName="JavaScript"
       userSecret="1234"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 }
