f2a3936b305974f0679934e06de21a8342a40d96
Bryan Roof <broof716@gmail.com>
2021-03-04 18:02:08 -0700

    creating login

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,57 +1,59 @@
 import { useState } from 'react';
-import { sendMessage, isTyping } from 'react-chat-engine';
 import { SendOutlined, PictureOutlined } from '@ant-design/icons';
- 
+import { sendMessage, isTyping } from 'react-chat-engine';
+
 const MessageForm = (props) => {
   const [value, setValue] = useState('');
-  const { chatId, creds } = props; 
+  const { chatId, creds } = props;
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const text = value.trim();
 
-    if(text.lengthn > 0) sendMessage(creds, chatId, { text });
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
+    }
 
     setValue('');
-  }
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-
-    isTyping(props, chatId);
-  }
+  };
 
   const handleUpload = (event) => {
-    sendMessage(creds, chatId, { files: event.target.files, text: '' })
-  }
+    sendMessage(creds, chatId, { files: event.target.files, text: '' });
+  };
 
   return (
     <form className="message-form" onSubmit={handleSubmit}>
-      <input 
+      <input
         className="message-input"
-        placeholder="Send a message ..."
+        placeholder="Send a message..."
         value={value}
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
-      <label htmlFor="upload-button"> 
+      <label htmlFor="upload-button">
         <span className="image-button">
-          <PictureOutlined  className="picture-icon"/>
+          <PictureOutlined className="picture-icon" />
         </span>
       </label>
-      <input 
+      <input
         type="file"
         multiple={false}
         id="upload-button"
         style={{ display: 'none' }}
-        onChange={handleUpload}
+        onChange={handleUpload.bind(this)}
       />
       <button type="submit" className="send-button">
-        <SendOutlined  className="send-icon" />
+        <SendOutlined className="send-icon" />
       </button>
     </form>
-  )
-}
+  );
+};
 
 export default MessageForm;
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,14 +1,16 @@
+
 const TheirMessage = ({ lastMessage, message }) => {
   const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
   return (
     <div className="message-row">
       {isFirstMessageByUser && (
-        <div 
+        <div
           className="message-avatar"
-          style={{ backgroundImage: `url(${message?.send?.avatar})`}}
+          style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
         />
       )}
-      {message?.attachments?.length > 0
+      {message.attachments && message.attachments.length > 0
         ? (
           <img
             src={message.attachments[0].file}
@@ -16,14 +18,14 @@ const TheirMessage = ({ lastMessage, message }) => {
             className="message-image"
             style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
           />
-        ) : (
-          <div className="message" style={{ float: 'left', backgroundColor: '#cabcdc', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+        )
+        : (
+          <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
             {message.text}
           </div>
-        )
-      }
+        )}
     </div>
-  )
-}
+  );
+};
 
 export default TheirMessage;
